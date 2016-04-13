fa02aa2eabf6b0f4056062fa13249b40db041695
TonyHerrera8 <herreraquinteroj@gmail.com>
2016-04-13 16:59:00 -0700

    Primera Version del proyecto

---
+++ Figura.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing; //agregar
+using System.Windows.Forms; //agregar
+
+namespace WindowsFormsApplication1
+{
+    abstract class Figura
+    {
+        public int X, Y;
+        public Pen pluma;
+        public Brush Brocha;
+        public int ancho, largo;
+
+        public Figura(int x,int y)
+        {
+            X=x; Y=y;
+            pluma = new Pen(Color.Red,2);
+            ancho = 10;
+            largo = 10;
+
+        }
+
+        public abstract void Dibuja(Form f);            
+    }
+
+    class Rectangulo : Figura
+    {
+        public Rectangulo(int x, int y) : base(x, y) 
+        {
+        }
+        public override void Dibuja(Form f)
+        {
+            Graphics g = f.CreateGraphics();
+            g.DrawRectangle(pluma, X, Y, ancho, largo);
+        }
+    }
+
+    class Circulo : Figura
+    {
+        public Circulo(int x, int y) : base(x, y)
+        {
+        }
+        public override void Dibuja(Form f)
+        {
+            Graphics g = f.CreateGraphics();
+            g.DrawEllipse(pluma, X, Y, ancho, largo);
+        }
+    }
+
+
+
+
+
+
+
+
+}
---
+++ Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        List<Figura> Figuras;
+
+        public Form1()
+        {
+            Figuras = new List<Figura>();
+            InitializeComponent();
+        }   
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                Circulo c = new Circulo(e.X, e.Y);
+                c.Dibuja(this);
+                Figuras.Add(c);
+
+ 
+            }
+            else if(e.Button == MouseButtons.Right)
+            {
+                Rectangulo r = new Rectangulo(e.X, e.Y);
+                r.Dibuja(this);
+                Figuras.Add(r);
+            }
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            foreach (Figura f in Figuras) //polimorfismo
+            {
+                f.Dibuja(this);
+            }
+        }
+    }
+}
